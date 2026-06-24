d1019842d77499979670c78e76b02d27fb3897e9
cerf007 <cerf02062007@gmail.com>
2026-06-24 09:29:01 -0600

    Subiendo configuraciones iniciales

---
+++ src/main/java/org/example/Razonamiento/run/Razonamiento.java
@@ -0,0 +1,16 @@
+package org.example.Razonamiento.run;
+
+import org.openxava.util.*;
+
+/**
+ * Execute this class to start the application.
+ */
+
+public class Razonamiento {
+
+	public static void main(String[] args) throws Exception {
+		//DBServer.start("Facturacion-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
+		AppServer.run("Razonamiento"); // Use AppServer.run("") to run in root context
+	}
+
+}
